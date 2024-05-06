d8859c05d52cc3197adfec32335fe34a904ddde0
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-05-06 03:19:26 +0300

    ??

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,18 +1,23 @@
 #include "sort.h"
 /**
  * swap_nodes - swapping
- *
+ * @node1: 1st
+ * @node2: 2nd
+ * @head: h pt
 */
-void swap_nodes(listint_t *node1, listint_t *node2)
+
+void swap_nodes(listint_t *node1, listint_t *node2, listint_t **head)
 {
-    if (node1->prev)
-        node1->prev->next = node2;
-    if (node2->next)
-        node2->next->prev = node1;
-    node2->prev = node1->prev;
-    node1->next = node2->next;
-    node1->prev = node2;
-    node2->next = node1;
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*head = node2;
+	if (node2->next)
+		node2->next->prev = node1;
+	node2->prev = node1->prev;
+	node1->next = node2->next;
+	node1->prev = node2;
+	node2->next = node1;
 }
 
 /**
@@ -23,24 +28,36 @@ void swap_nodes(listint_t *node1, listint_t *node2)
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *h, *mov;
+	listint_t *temp, *mov;
 
 	if (list && *list && (*list)->next)
 	{
-		h = *list;
-		mov = h;
-		for (; mov->next; mov = mov->next)
+		mov = *list;
+		for (; (mov && mov->next); mov = mov->next)
 		{
-			if (mov->prev)
-			{
-				if (mov->prev->n > mov->n)
-					swap_nodes(mov->prev, mov);
-			}
 			if (mov->n > mov->next->n)
-				swap_nodes(mov, mov->next);
-
-			print_list(h);
+			{
+				swap_nodes(mov, mov->next, list);
+				print_list(*list);
+				temp = mov->prev;
+			}
+			else
+				temp = mov;
+			while (1)
+			{
+				if (temp && temp->prev)
+				{
+					if (temp->prev->n > temp->n)
+					{
+						swap_nodes(temp->prev, temp, list);
+						print_list(*list);
+						continue;
+					}
+					else
+						break;
+				}
+				break;
+			}
 		}
 	}
-
 }
